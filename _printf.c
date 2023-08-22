9b01681bd941eda2453ccce7c106bc43b05745d5
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2023-08-22 10:45:50 +0100

    Task 0 One More Time

---
+++ _printf.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+* _printf - a skeleton version of printf
+* @format: default string literal
+* Return: count
+*/
+
+int _printf(const char *format, ...)
+{
+	va_list list;
+	int count = 0, i = 0;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(list, format);
+
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			count += rep_format(format[++i], list);
+		}
+		else
+		{
+			count += _putchar(format[i]);
+		}
+		i++;
+	}
+	va_end(list);
+	return (count);
+}
+
