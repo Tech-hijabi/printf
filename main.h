d7c040f32fa12ecfedb4be181f2431611b20ef7d
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2023-08-22 22:40:15 +0100

    Tasks 2 and 3

--- main.h
+++ main.h
@@ -14,6 +14,22 @@ int rep_format(char specifier, va_list list);
 
 int _putchar(char c);
 
+int print_string(char *str);
+
+int hex(int n, int isUppercase);
+
 int _putint(int n);
+
+int _putbin(unsigned int n);
+
+int _puthex(unsigned int n);
+
+int _putHex(unsigned int n);
+
+int _putoct(unsigned int n);
+
+int _unint(unsigned int n);
+
+char *_itoa(int value, char *buffer, int radix);
 
 #endif
